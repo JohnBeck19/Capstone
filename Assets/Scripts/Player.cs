f0945a9f8d6af5bc6eabf63e27657299fd99b7a3
JohnBeck19 <jbeck@student.neumont.edu>
2024-10-13 17:10:28 -0600

    DASH

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -13,6 +13,14 @@ public class Player : MonoBehaviour
     [SerializeField] Animator Animator;
     private facing dir = facing.BACKWARD;
 
+    //dash 
+    private Vector3 dashDirection;
+    private bool isDashing = false;
+    private float dashTimer = 0f;
+    private float dashCD = 0f;
+    [SerializeField] float dashDistance = 20f;
+    [SerializeField] float dashCooldown = 2.0f;
+
 
     // Update is called once per frame
     void Update()
@@ -54,42 +62,76 @@ public class Player : MonoBehaviour
                 Animator.SetTrigger("SideIdle");
             }
         }
-
+        //set direction
         if (Input.GetKey(KeyCode.W) && Input.GetKey(KeyCode.D)) dir = facing.FORWARDRIGHT;
         if (Input.GetKey(KeyCode.W) && Input.GetKey(KeyCode.A)) dir = facing.FORWARDLEFT;
         if (Input.GetKey(KeyCode.S) && Input.GetKey(KeyCode.A)) dir = facing.BACKWARDLEFT;
         if (Input.GetKey(KeyCode.S) && Input.GetKey(KeyCode.D)) dir = facing.BACKWARDRIGHT;
 
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.Space) || isDashing)
         {
-            switch (dir)
+            if (dashCD >= 0)
             {
-                case facing.LEFT:
-                    transform.position += Vector3.left * 5;
-                    break;
-                case facing.RIGHT:
-                    transform.position += Vector3.right * 5;
-                    break;
-                case facing.FORWARD:
-                    transform.position += transform.forward * 5;
-                    break;
-                case facing.BACKWARD:
-                    transform.position += -transform.forward * 5;
-                    break;
-                case facing.FORWARDRIGHT:
-                    transform.position += (transform.forward + Vector3.right).normalized *5;
-                    break;
-                case facing.FORWARDLEFT:
-                    transform.position += (transform.forward + Vector3.left).normalized * 5;
-                    break;
-                case facing.BACKWARDRIGHT:
-                    transform.position += (-transform.forward + Vector3.right ).normalized * 5;
-                    break;
-                case facing.BACKWARDLEFT:
-                    transform.position += (-transform.forward + Vector3.left).normalized * 5;
-                    break;
+                ;
             }
+            else if (isDashing)
+            {
+                dash(dashDirection * Time.deltaTime * dashDistance);
+                dashTimer -= Time.deltaTime;
+                if (dashTimer < 0)
+                {
+                    isDashing = false;
+                    dashCD = dashCooldown;
+                }
+            }
+            else
+            {
+                isDashing = true;
+                setDashDirection();
+                dashTimer = .15f;
+            }
+        }
+        if (dashCD >= 0)
+        {
+            dashCD -= Time.deltaTime;
+        }
+    }
 
+    void dash(Vector3 DashDistance)
+    { 
+        transform.position += DashDistance;
+    }
+
+
+
+    private void setDashDirection()
+    {
+        switch (dir)
+        {
+            case facing.LEFT:
+                dashDirection = Vector3.left;
+                break;
+            case facing.RIGHT:
+                dashDirection = Vector3.right;
+                break;
+            case facing.FORWARD:
+                dashDirection = transform.forward;
+                break;
+            case facing.BACKWARD:
+                dashDirection = -transform.forward;
+                break;
+            case facing.FORWARDRIGHT:
+                dashDirection = (transform.forward + Vector3.right).normalized;
+                break;
+            case facing.FORWARDLEFT:
+                dashDirection = (transform.forward + Vector3.left).normalized;
+                break;
+            case facing.BACKWARDRIGHT:
+                dashDirection = (-transform.forward + Vector3.right).normalized;
+                break;
+            case facing.BACKWARDLEFT:
+                dashDirection = (-transform.forward + Vector3.left).normalized;
+                break;
         }
     }
 }
