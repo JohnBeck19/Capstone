6fed1a60d8ba10bd1af8b1fe1bd2542862810abb
JohnBeck19 <jbeck@student.neumont.edu>
2024-10-13 23:36:41 -0600

    vignette added to shadow mechanic

--- Assets/Scripts/ShadowDetection.cs
+++ Assets/Scripts/ShadowDetection.cs
@@ -1,24 +1,58 @@
 using UnityEditor.SearchService;
 using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.Rendering.Universal;
 
 public class ShadowDetector : MonoBehaviour
 {
     public Light sun;
+    [SerializeField] Volume volume;
+    private Vignette vignette;
+    private float vignetteIntensity;
+    private Color vignetteColor;
+    [SerializeField] Color sunColor;
     private Light[] lights;
+    
     void Start()
     {
         lights = FindObjectsOfType<Light>();
+        volume.profile.TryGet<Vignette>(out vignette);
     }
     void Update()
     {
 
-        if (isInSunlight(transform.position) && isInLight(transform.position))
+        if (isInSunlight(transform.position) && isNotInLight(transform.position))
         {
             Debug.Log("is in shadow");
+            if (vignette.intensity.value > 0.0f)
+            {
+                vignette.intensity.value -= Time.deltaTime;
+            }
+            else
+            {
+                vignette.intensity.value = 0.0f;
+            }
+            
+            
         }
         else
         {
             Debug.Log("is in the light");
+            
+            vignette.color.value = vignetteColor;
+            if (Mathf.Abs(vignette.intensity.value - vignetteIntensity) < 0.05f)
+            {
+                vignette.intensity.value = vignetteIntensity;
+            }
+            else if (vignette.intensity.value > vignetteIntensity)
+            {
+                vignette.intensity.value -= Time.deltaTime;
+            }
+            else if (vignette.intensity.value < vignetteIntensity)
+            {
+                vignette.intensity.value += Time.deltaTime;
+            }
+
         }
     }
 
@@ -26,7 +60,7 @@ public class ShadowDetector : MonoBehaviour
     {
         // Calculate the direction from the object to the light source
         Vector3 lightDirection = -sun.transform.forward;
-
+        
         // Cast a ray from the object in the direction of the light
         Ray ray = new Ray(position, lightDirection);
         Debug.DrawRay(position, lightDirection*100,Color.yellow);
@@ -37,27 +71,32 @@ public class ShadowDetector : MonoBehaviour
         {
             return true;
         }
-
+        vignetteColor = sunColor;
+        vignetteIntensity = 0.5f;
         return false;
     }
 
-    bool isInLight(Vector3 position)
+    bool isNotInLight(Vector3 position)
     {
         foreach (Light light in lights)
         {
             if (light == sun) continue;
             if (Mathf.Abs(Vector3.Distance(light.transform.position, position)) <= light.range)
             {
+                float lightDistance = Vector3.Distance(light.transform.position, position);
                 Vector3 lightDirection = (light.transform.position - position).normalized;
                 Ray ray = new Ray(position, lightDirection);
                 Debug.DrawRay(position, lightDirection * 100, Color.magenta);
                 RaycastHit hit;
 
-                if (Physics.Raycast(ray, out hit, Vector3.Distance(light.transform.position, position)))
+                if (Physics.Raycast(ray, out hit, lightDistance))
                 {
                     return true;
                 }
-                else { return false; }
+                else {
+                    vignetteColor = light.color;
+                    vignetteIntensity = 1/lightDistance +0.2f;
+                    return false; }
             }
         }
         return true;   
